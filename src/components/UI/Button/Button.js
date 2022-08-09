59023139e1fa42df00da335cfcaee007653230c7
Dylan Cramer <dcrame2@gmail.com>
2022-08-09 08:14:44 -0500

    adding project

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Button.css';
+import styled from 'styled-components'
+
+const Button 
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
+
+export default Button;
